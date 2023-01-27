81191c9e268208c72773967f4634c0ffa8462342
Vitor Sanches Ferreira <112482028+SanchesVitor@users.noreply.github.com>
2023-01-27 12:33:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const form = document.querySelector('form');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-  takePills: ["01-03", "01-09"],
-  journal: ["01-02", "01-04"]
-};
+button.addEventListener('click', add);
 
-nlwSetup.setData(data);
-nlwSetup.load()
+function add() {
+  const today = "01/01";
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  nlwSetup.addDay(today);
+
+}
+
+// const data = {
+//   run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
+//   takePills: ["01-03", "01-09"],
+//   journal: ["01-02", "01-04"]
+// };
+
+// nlwSetup.setData(data);
+// nlwSetup.load()
